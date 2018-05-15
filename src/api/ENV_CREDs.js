031bcba76b785116ed7ae96d5452e6214f828625
Kangze Huang <kangzeroo@gmail.com>
2018-05-15 02:51:53 -0400

    integrated passwordless signin

--- src/api/ENV_CREDs.js
+++ src/api/ENV_CREDs.js
@@ -9,6 +9,11 @@ export const FIREBASE_CREDS = {
   storageBucket: "dev-landlordai-8221e.appspot.com",
   messagingSenderId: "667543210133"
 }
+export const AWS_FEDERATED_IDENTITY_ENV = 'us-east-1:1fce3ff3-ff33-419a-8e5e-c377bc3a7d0d'
+export const PASSWORDLESS_LOGIN_REDIRECT = 'https://localhost:8888/passwordless'
+export const AUTH0_CLIENT_ID = 'WmvAjvnb1XCvzoZIlv7fUYRR4MgmhOhK'
+export const AUTH0_DOMAIN = 'renthero.auth0.com'
+
 
 // STAGING
 // export const FIREBASE_CREDS = {
