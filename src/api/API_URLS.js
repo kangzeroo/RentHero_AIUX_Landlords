2fbbe5bf1a0786c8b7e41fc2f376fc8c55ab7475
Kangze Huang <kangzeroo@gmail.com>
2018-05-23 23:00:13 -0400

    thumbs up fixed css

--- src/api/API_URLS.js
+++ src/api/API_URLS.js
@@ -4,13 +4,13 @@
 // LOCAL
 
 // DEV
-export const DIALOGFLOW_MS = 'https://6482dbc5.ngrok.io'
+// export const DIALOGFLOW_MS = 'https://6482dbc5.ngrok.io'
 // export const FCM_MS = 'https://a72a399c.ngrok.io'
 // export const TENANT_AD_MS = 'https://localhost:8202'
 //
 
 // STAGING
-// export const DIALOGFLOW_MS = 'https://renthero.host:8302'
+export const DIALOGFLOW_MS = 'https://renthero.host:8302'
 export const FCM_MS = 'https://renthero.host:8303'
 export const TENANT_AD_MS = 'https://renthero.host:8202'
 
--- src/components/modules/QuickReply.js
+++ src/components/modules/QuickReply.js
@@ -532,6 +532,10 @@ const comStyles = (full) => {
       borderRadius: '20px',
       border: '#2faded solid 2px',
       color: '#2faded',
+      display: 'flex',
+      flexWrap: 'wrap',
+      whiteSpace: 'normal',
+      height: 'auto'
     },
     prettyInput: {
       borderRadius: '25px !important',
